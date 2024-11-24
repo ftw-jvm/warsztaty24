5fd17059212e289b3fd063163f851cfac3e82796
ftw-jvm <anszlus-szczypac0w@icloud.com>
2024-11-24 15:37:24 +0100

    kafka intro

--- workshop24/src/main/java/pl/sii/digital/workshop24/Workshop24Application.java
+++ workshop24/src/main/java/pl/sii/digital/workshop24/Workshop24Application.java
@@ -1,13 +1,30 @@
 package pl.sii.digital.workshop24;
 
+import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.config.TopicBuilder;
 
 @SpringBootApplication
 public class Workshop24Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Workshop24Application.class, args);
+	}
+
+	@Bean
+	public NewTopic topic(){
+		return TopicBuilder.name("javatopic")
+				.partitions(1)
+				.replicas(1)
+				.build();
+	}
+
+	@KafkaListener(id = "listener1", topics = "javatopic")
+	public void listen(String inbound) {
+		System.out.println("Got this from topic: " + inbound);
 	}
 
 }
