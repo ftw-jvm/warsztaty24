9ddd7b135b1e589a5875f4314f363376453e167e
ftw-jvm <anszlus-szczypac0w@icloud.com>
2024-11-24 16:08:30 +0100

    more kafka

--- workshop24/src/main/java/pl/sii/digital/workshop24/ShellComponent.java
+++ workshop24/src/main/java/pl/sii/digital/workshop24/ShellComponent.java
@@ -3,13 +3,19 @@ package pl.sii.digital.workshop24;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.shell.standard.ShellMethod;
+import org.springframework.shell.standard.ShellOption;
 
 @org.springframework.shell.standard.ShellComponent
 public class ShellComponent {
     
     @Autowired
     StocksClient stocksClient;
+
+    @Autowired
+    KafkaTemplate<String, String> kafkaTemplate;
 
     @ShellMethod(key = "hello")
     public String getHello(){
@@ -21,5 +27,9 @@ public class ShellComponent {
         return Arrays.toString(stocksClient.getStocksData());
     }
 
+    @ShellMethod(key = "send")
+    public void sendToTopic(String message, @ShellOption(defaultValue = "javatopic") String topicname){
+        kafkaTemplate.send(topicname, message);
+    }
 
 }
